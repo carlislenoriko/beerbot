8005247b64b478471aacb03cd885394516be0480
cfuj <cfujiyoshi@gmail.com>
2018-07-25 21:30:22 -0700

    Updated scraper file with comments and cleaned up

--- scraper.py
+++ scraper.py
@@ -5,25 +5,18 @@ import json
 import tweepy
 import secrets
 
+# Twitter auth
 auth = tweepy.OAuthHandler(secrets.consumer_key, secrets.consumer_secret)
 auth.set_access_token(secrets.access_token, secrets.access_token_secret)
-
 api = tweepy.API(auth)
 
+# Web scraping
 page = requests.get('https://fieldworkbrewing.com/berkeley/')
 tree = html.fromstring(page.content)
 
 current_beer = tree.xpath('//ul[@class="taplist-content"]//li[@class="beer ontap "]//div[@class="beer-content"]')
 
-# name = all_beer[0].xpath('h1/text()')[0]
-
-# description = tree.xpath('//ul[@class="taplist-content"]//li[@class="beer ontap "]//div[@class="beer-content"]//h2/text()')
-
-# description = all_beer[0].xpath('h2/text()')[0]
-
-# https://doc.scrapy.org/en/xpath-tutorial/topics/xpath-tutorial.html
-# https://3583bytesready.net/2016/08/17/scraping-data-python-xpath/
-
+# If JSON exists, is opened and put in old_beers
 old_beers = []
 if os.path.isfile("beers.json"):
   old_beers = json.load(open('beers.json'))
@@ -31,19 +24,19 @@ if os.path.isfile("beers.json"):
 vowels = ('A', 'E', 'I', 'O', 'U')
 name_list = []
 for beer in current_beer:
+  # Names and descriptions taken from website
   name = beer.xpath('h1/text()')[0]
   description = beer.xpath('h2/text()')[0]
   if name not in old_beers:
+    # Names not found in old_beers tweeted with vowel handling
     if description.startswith(vowels):
       api.update_status(f'NEW BEER ALERT: {name} is an {description}!!!')
     else:
-      api.update_status(f'NEW BEER ALERT: {name} is a {description}!!!')   
+      api.update_status(f'NEW BEER ALERT: {name} is a {description}!!!')
     print("NEW BEER OH SHIT")
   name_list.append(name)
 
-# json_attempt = json.dumps(name_list)
-# print(json_attempt)
-
+# Beer JSON either opened or created
 with open('beers.json', 'w') as outfile:
   json.dump(name_list, outfile)
 
